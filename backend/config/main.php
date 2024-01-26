f1e7482607ff7fc0449b82ad114be3ec68bd4808
VICTUS <yusupovdoniyor54@gmail.com>
2024-01-26 11:12:44 +0500

    fixing main config

--- backend/config/main.php
+++ backend/config/main.php
@@ -18,7 +18,7 @@ return [
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
-            'baseUrl' => '',
+            'baseUrl' => '/admin',
         ],
         'user' => [
             'identityClass' => 'common\models\User',

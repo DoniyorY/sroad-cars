3eab003cc10b18b9986133371dc54ac78afca9fd
VICTUS <yusupovdoniyor54@gmail.com>
2024-01-31 14:54:24 +0500

    Revert "Cars Index Page"
    This reverts commit 87adc8a3

--- frontend/views/cars/view.php
+++ frontend/views/cars/view.php
@@ -5,6 +5,7 @@ use yii\helpers\Html;
 
 $lang = Yii::$app->language;
 $baseUrl = Yii::$app->request->baseUrl;
+$this->title = 'Автобус HIGER';
 ?>
 <div class="cars_banner">
     <img src="<?= $baseUrl . '/img/breadcrumbs_banner.png' ?>" alt="">
@@ -12,10 +13,23 @@ $baseUrl = Yii::$app->request->baseUrl;
 <section class="cars_view">
     <div class="row w-100">
         <div class="col-md-5 p-0 left_cars_view">
-            
+            <div class="left_title">
+                <h1>Автобус HIGER</h1>
+            </div>
+            <div class="left_desc">
+                <p>
+                    Наши автобусы Higer — это идеальное сочетание простора и комфорта. <br>
+                    Благодаря уютному кожаному салону, ваша поездка будет особенно приятной и расслабляющей. <br>
+                    Этот автобус идеально подходит для организации трансферов и захватывающих поездок по
+                    Самарканду, Бухаре, Хиве, Нурате и Ташкенту, чтобы вы могли насладиться всей
+                    красотой и богатством Узбекистана. <br>
+                    Независимо от вашей цели, наши автобусы Higer обеспечат стиль, комфорт и надежность в
+                    каждой поездке.
+                </p>
+            </div>
         </div>
         <div class="col-md-7 right_cars_view">
-            <img src="<?=$baseUrl.'/img/cars_view.png'?>" alt="">
+            <img src="<?= $baseUrl . '/img/cars_view.png' ?>" alt="">
         </div>
     </div>
 </section>

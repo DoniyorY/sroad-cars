c2a151f4ffd067bc0603b34b09678a5d339f787b
VICTUS <yusupovdoniyor54@gmail.com>
2024-02-08 17:55:39 +0500

    Owl card changes

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -153,6 +153,7 @@ $baseUrl = Yii::$app->request->baseUrl;
             <div class="col-md-12">
                 <div class="owl-carousel owl-theme">
                     <?php foreach ($owl_cars as $item):
+                        $conn=\common\models\Connector::find()->where(['car_id'=>$item->id])->orderBy(['price'=>SORT_ASC])->one();
                         $main = CarGallery::findOne(['cars_id' => $item->id, 'type_id' => 0]);
                         $secondary = CarGallery::findOne(['cars_id' => $item->id, 'type_id' => 1]);
                         if (is_null($main) or is_null($secondary)) {
@@ -165,12 +166,15 @@ $baseUrl = Yii::$app->request->baseUrl;
                         switch ($lang) {
                             case 'ru':
                                 $capacity = $item->capacity . ' Мест';
+                                $price='<strong>Цена от: </strong>' .Yii::$app->formatter->asDecimal($conn->price,0) . ' UZS';
                                 break;
                             case 'en':
                                 $capacity = "Capacity: $item->capacity";
+                                $price='<strong>Price: </strong>' .Yii::$app->formatter->asDecimal($conn->price,0) . ' UZS';
                                 break;
                             case 'uz':
                                 $capacity = "$item->capacity Joy";
+                                $price=Yii::$app->formatter->asDecimal($conn->price,0) . ' <strong> So`mdan</strong>';
                                 break;
                         }
                         ?>
@@ -189,17 +193,17 @@ $baseUrl = Yii::$app->request->baseUrl;
                                 </div>
                                 <div class="card_image2">
                                     <div class="card_info">
-                                        <div class="capacity"><strong>Вместимость:</strong> 99</div>
-                                        <div class="baggage"><strong>Багаж:</strong> 99</div>
+                                        <div class="capacity"><strong>Вместимость:</strong> <?=$item->capacity?></div>
+                                        <div class="baggage"><strong>Багаж:</strong> <?=$item->baggage?></div>
                                     </div>
-                                    <p style="mt10">
+                                    <p style="margin-top: 10px;">
                                         <?= mb_substr($item->{"content_$lang"}, 0, 175) . '...';?>
                                     </p>
                                    <!-- <img src="<?php /*= $secondary */?>" alt="">-->
                                 </div>
                                 <div class="card_footer">
                                     <div class="card_price">
-                                        <strong>Цена от:</strong> 1 000 000 UZS
+                                        <?=$price?>
                                     </div>
                                     <a class="btn btn-sroad" href="<?= Url::to(['cars/view', 'id' => $item->id]) ?>">
                                         Узнать подробнее

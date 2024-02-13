60b277f491c800eb1ca0701702ef46ef478c9c30
DoniyorY <87862961+DoniyorY@users.noreply.github.com>
2024-02-13 11:36:32 +0500

    add card svg

--- frontend/views/cars/view.php
+++ frontend/views/cars/view.php
@@ -4,7 +4,7 @@ use yii\helpers\Url;
 
 $lang = Yii::$app->language;
 $baseUrl = Yii::$app->request->baseUrl;
-$this->title = 'Автобус HIGER';
+$this->title = $model->category->{"name_$lang"} . ' ' .$model->{"name_$lang"};
 ?>
 <div class="cars_banner">
     <img src="<?= $baseUrl . '/img/breadcrumbs_banner.png' ?>" alt="">
@@ -13,17 +13,11 @@ $this->title = 'Автобус HIGER';
     <div class="row w-100">
         <div class="col-md-5 p-0 left_cars_view">
             <div class="left_title">
-                <h1>Автобус HIGER</h1>
+                <h1><?=$this->title?></h1>
             </div>
             <div class="left_desc">
                 <p>
-                    Наши автобусы Higer — это идеальное сочетание простора и комфорта. <br>
-                    Благодаря уютному кожаному салону, ваша поездка будет особенно приятной и расслабляющей. <br>
-                    Этот автобус идеально подходит для организации трансферов и захватывающих поездок по
-                    Самарканду, Бухаре, Хиве, Нурате и Ташкенту, чтобы вы могли насладиться всей
-                    красотой и богатством Узбекистана. <br>
-                    Независимо от вашей цели, наши автобусы Higer обеспечат стиль, комфорт и надежность в
-                    каждой поездке.
+                    <?=$model->{"content_$lang"}?>
                 </p>
             </div>
         </div>
